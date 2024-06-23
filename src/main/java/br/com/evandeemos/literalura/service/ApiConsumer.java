db1bc826cbe8002f98b12d1cc170df5a0f4e7e78
EvandeeMoS <moisesevander@gmail.com>
2024-06-23 08:58:48 -0300

    Add ApiConsumer, so we can get data from apis

---
+++ src/main/java/br/com/evandeemos/literalura/service/ApiConsumer.java
@@ -0,0 +1,21 @@
+package br.com.evandeemos.literalura.service;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.URI;
+
+public class ApiConsumer {
+    public String fetch(String url) {
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            return response.body();
+        }
+        catch (InterruptedException | IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
